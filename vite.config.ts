d613bc3c301f79cee2d5f3f4afe777c21b4abb4c
igstX4 <programx41@gmail.com>
2025-04-18 09:16:42 +0300

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -9,23 +9,23 @@ export default defineConfig({
     headers: {
       'ngrok-skip-browser-warning': 'true',
     },
-    proxy: {
-      '/api': {
-        target: 'https://dsfqq.ru/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        }
-      }
-    }
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://dsfqq.ru/api',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ''),
+    //     configure: (proxy, _options) => {
+    //       proxy.on('error', (err, _req, _res) => {
+    //         console.log('proxy error', err);
+    //       });
+    //       proxy.on('proxyReq', (proxyReq, req, _res) => {
+    //         console.log('Sending Request to the Target:', req.method, req.url);
+    //       });
+    //       proxy.on('proxyRes', (proxyRes, req, _res) => {
+    //         console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+    //       });
+    //     }
+    //   }
+    // }
   }
 })
