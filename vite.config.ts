f558a6a3c04b2ea528487ae8778804b099652de4
igstX4 <programx41@gmail.com>
2025-04-18 09:06:45 +0300

    asass

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,33 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-
- export default defineConfig({
-   plugins: [react()],
-   server: {
+export default defineConfig({
+  plugins: [react()],
+  server: {
     allowedHosts: ['.ngrok-free.app'],
     headers: {
       'ngrok-skip-browser-warning': 'true',
     },
-     proxy: {
-       '/api': {
-         target: 'https://dsfqq.ru/api',
-         changeOrigin: true,
-         rewrite: (path) => path.replace(/^\/api/, ''),
-         configure: (proxy, _options) => {
-           proxy.on('error', (err, _req, _res) => {
-             console.log('proxy error', err);
-           });
-           proxy.on('proxyReq', (proxyReq, req, _res) => {
-             console.log('Sending Request to the Target:', req.method, req.url);
-           });
-           proxy.on('proxyRes', (proxyRes, req, _res) => {
-             console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-           });
-         }
-       }
-     }
-   }
- })
+    proxy: {
+      '/api': {
+        target: 'https://dsfqq.ru/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        }
+      }
+    }
+  },
+  build: {
+    sourcemap: false
+  }
+})
